1790f1da9322dafde39dab07f0648f2c52a1ee26
Lee Barnard <lee.barnard@man.com>
2016-06-10 15:11:54 +0100

    added param to parallel call for threads.

--- Digit Recognizer/DigitRecognizer/Program.cs
+++ Digit Recognizer/DigitRecognizer/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using DigitRecognizer.Core;
 using DigitRecognizer.Core.DistanceAlgorithms;
 
